53afbd8fda484cd99f69404281d12883f2001ce5
MichaelDavisIII <business.davis.mike1910@gmail.com>
2017-06-12 17:49:44 -0500

    UD4
    6/12/2017

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import { connect } from 'react-redux';
+// Main Index.js file for the app.//
+
+import React, { Component } from 'react';
+//import { connect } from 'react-redux';
 import { Scene, Router } from 'react-native-router-flux';
-import { View } from 'react-native';
-import {
-    // Further imports here.
-    HomeS,
-    S1,
-    S2,
-    S3,
-    S4,
-    S5,
-} from './src/components/screens';
+//import { View } from 'react-native';
+import { AppRegistry } from 'react-native';
+import { HomeS } from './src/components/screens';
+import { S1 } from './src/components/screens';
+import { S2 } from './src/components/screens';
+import { S3 } from './src/components/screens';
+import { S4 } from './src/components/screens';
+import { S5 } from './src/components/screens';
+
+export default class CT extends Component {
+    render() {
+        return(
+            <Router>
+                <Scene key="root">
+                    <Scene key = "HomeS" Component = { HomeS } title = "HomeS" initial = { true } />
+                    <Scene key = "S1" Component = { S1 } title = "S1" />
+                    <Scene key = "S2" Component = { S2 } title = "S2" />
+                    <Scene key = "S2" Component = { S2 } title = "S2" />
+                    <Scene key = "S3" Component = { S3 } title = "S3" />
+                    <Scene key = "S4" Component = { S4 } title = "S4" />
+                    <Scene key = "S5" Component = { S5 } title = "S5" />
+                </Scene>
+            </Router>
+        );
+    }
+}
+
+
+
+
+AppRegistry.registerComponent(`CT`, () => CT);
